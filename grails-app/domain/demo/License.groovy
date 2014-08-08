401441f96337155c51c76eaac9128c3bf0e05c5c
Jove Zhong <jzhong@splunk.com>
2014-08-08 19:59:26 +0800

    make Domain class as REST resource

--- grails-app/domain/demo/License.groovy
+++ grails-app/domain/demo/License.groovy
@@ -1,7 +1,18 @@
 package demo
 
+import grails.rest.*
+
+@Resource(uri='/licenses',formats=['json', 'xml'])
 class License {
 
+    String user
+    String email
+    String sn
+    Date generatedOn=new Date()
+
     static constraints = {
+        user blank:false
+        email blank:false
+        sn blank:false
     }
 }
